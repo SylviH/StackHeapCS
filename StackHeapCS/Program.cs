5122922dab9c6e514fef903d622c2950f9cfcfba
Michell Cronberg <devcronberg@gmail.com>
2018-01-22 15:56:19 +0100

    url

--- StackHeapCS/Program.cs
+++ StackHeapCS/Program.cs
@@ -5,6 +5,8 @@ namespace StackHeapCS
     {
         static void Main(string[] args)
         {
+            // Get code from https://github.com/devcronberg/StackHeapCS
+
             Person p1 = new Person();
             p1.name = "a";
             p1.age = 10;
@@ -29,16 +31,18 @@ namespace StackHeapCS
             Console.WriteLine("p1 = name " + p1.name + " age " + p1.age);
             Console.WriteLine("p2 = name " + p2.name + " age " + p2.age);
 
-            // Make sure Person is a struct, run program, view and understand output
-            // Make sure Person is a class, run program, view and understand output
-            // Make sure you understand the difference
+            // - Make sure Person is a struct, 
+            //   run program, view and understand output
+            // - Make sure Person is a class, 
+            //   run program, view and understand output
+            // - Make sure you understand the difference
 
             if (System.Diagnostics.Debugger.IsAttached)
                 Console.ReadKey();
         }
     }
-    struct Person
-    { // or class Person
+    struct Person // or class Person
+    { 
         public string name;
         public int age;
     }
